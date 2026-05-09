fc47bc199dcc94628460c414df5118fe2c374e19
cherrymaxx0 <ariksab007@gmail.com>
2026-05-09 17:37:03 +0600

    Finally done 1342a

--- 1342a.cpp
+++ 1342a.cpp
@@ -3,18 +3,29 @@
 using namespace std;
 
 int main(void) {
-    long long int t,x,y,a,b,sum=0;
+    long long int t,x,y,a,b;
     cin>>t;
     while(t--){
-    while(t--){
+        long long int sum=0;
         int n=0;
         cin>>x>>y;
         cin>>a>>b;
-        if(b<a){
-            n=abs(x-abs(x-y));
-        } else if(a<b){
-            n=abs(x-abs(x-y));
-
+        if(x>=y) n=abs(x-abs(x-y));
+        else n=abs(y-abs(x-y));
+        if(a<=b){
+            long long int sum1=0;
+            long long int sum2=0;
+            for (int i=0;i<n;i++) sum1+=b;
+            n=abs(x-y);
+            for(int i=0;i<n;i++) sum1+=a;
+            n=x+y;
+            for(int i=0;i<n;i++) sum2+=a;
+            if (sum1<=sum2) sum=sum1;
+            else sum=sum2;
+        } else if(b<a){
+            for (int i=0;i<n;i++) sum+=b;
+            n=abs(x-y);
+            for(int i=0;i<n;i++) sum+=a;
         }
         cout<<sum<<endl;
     }
