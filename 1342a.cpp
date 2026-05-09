a535594d8ba1964f75d0f026685061c37628a7c3
cherrymax0 <ariksab007@gmail.com>
2026-05-09 13:25:15 +0600

    More changes

--- 1342a.cpp
+++ 1342a.cpp
@@ -4,25 +4,16 @@ using namespace std;
 int main(void) {
     int t,x,y,a,b,sum=0;
     cin>>t;
-    while(t){
+    while(t--){
         int n=0;
         cin>>x>>y;
         cin>>a>>b;
-        if (x>y) {
-            for(int i=0;i<x-y;i++){
-                sum+=a;
-            }
-            n=y;
-        } else if (y>x) {
-            for(int i=0;i<y-x;i++) {
-                sum+=a;
-            }
-            n=x;
-        } 
-        if(n) {
-            for(int i=0;i<n;i++) sum+=b;
-        } else sum=0;
-        t--;
+        if(b<a){
+            n=abs(x-abs(x-y));
+        } else if(a<b){
+            n=abs(x-abs(x-y));
+
+        }
         cout<<sum<<endl;
     }
 }
